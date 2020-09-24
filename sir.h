5e5bd8d8f3cdaac7db9d097f4fc58ee1a0803a08
Petter Holme <pttrhlm@gmail.com>
2020-09-24 23:32:58 +0900

    missing argument in prototype

--- sir.h
+++ sir.h
@@ -53,6 +53,6 @@ extern void read_data (FILE *);
 // pcg_rnd.c
 extern uint16_t pcg_16 ();
 extern uint32_t pcg_32 ();
-extern uint32_t pcg_32_bounded ();
+extern uint32_t pcg_32_bounded (uint32_t);
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
